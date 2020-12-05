7d61c6629df1ca4ce937705a02931a81fc29032e
Linh Dang <linh.dang01@gmail.com>
2020-12-04 23:23:20 -0800

    minor changes for formatting

--- db/postgreSQL_seed.js
+++ db/postgreSQL_seed.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
 const fs = require('fs');
+const faker = require('faker');
 // const path = require('path');
 // const { Pool, Client } = require('pg');
 
@@ -45,14 +46,13 @@ const baths = [2, 3, 4, 5, 6];
 const sq_footage = [3500, 4000, 4500, 5000, 5500];
 const address = ['Presidio Ter', 'Sea Cliff Ave', 'Glenbrook Ave', 'Marina Blvd', 'Scott St', 'Filbert St'];
 const neighborhood = ['Pacific Heights, San Francisco, CA', 'Bernal Heights, San Francisco, CA', 'Noe Valley, San Francisco, CA', 'Castro, San Francisco, CA', 'Seacliff, San Francisco, CA', 'Clarendon Heights, San Francisco, CA'];
-const names = ['Keira Gothard', 'Charita Kinlaw', 'Talia Beutler', 'Eulah Winbush', 'Clyde Najera', 'Maura Goodwin', 'Kathe Westray', 'Louie Bubb', 'Ghislaine Teston', 'Maribel Carwell'];
 
-var numOfListings = 10;
-var numOfUsers = 10;
+var numOfListings = 10000000;
+var numOfUsers = 1000000;
 
 const writeListings = fs.createWriteStream('seedListings.csv');
 writeListings.write('id;price;bedrooms;baths;sq_footage;address;neighborhood\n', 'utf8');
-const generateRandomListingData = (writer, encoding, callback) => {
+const generateRandomListingsData = (writer, encoding, callback) => {
   let id = 0;
   let i = numOfListings;
   function write() {
@@ -79,18 +79,18 @@ const generateRandomListingData = (writer, encoding, callback) => {
   }
   write();
 };
-generateRandomListingData(writeListings, 'utf8', (err) => {
+generateRandomListingsData(writeListings, 'utf8', (err) => {
   if (err) {
     console.log('err', err);
   } else {
-    console.log('listing data generated');
+    console.log('listings data generated');
     writeListings.end();
   }
 });
 
 const writeUsers = fs.createWriteStream('seedUsers.csv');
 writeUsers.write('id;name\n', 'utf8');
-const generateRandomUserData = (writer, encoding, callback) => {
+const generateRandomUsersData = (writer, encoding, callback) => {
   let id = 0;
   let i = numOfUsers;
   function write() {
@@ -98,7 +98,7 @@ const generateRandomUserData = (writer, encoding, callback) => {
     do {
       i--;
       id++;
-      const userName = `${names[Math.floor(Math.random() * 10)]}`
+      const userName = faker.name.findName();
       const data = `${id};${userName}\n`
       if (i === 0) {
         writer.write(data, encoding, callback)
@@ -112,18 +112,18 @@ const generateRandomUserData = (writer, encoding, callback) => {
   }
   write();
 };
-generateRandomUserData(writeUsers, 'utf8', (err) => {
+generateRandomUsersData(writeUsers, 'utf8', (err) => {
   if (err) {
     console.log('err', err);
   } else {
-    console.log('user data generated');
+    console.log('users data generated');
     writeUsers.end();
   }
 });
 
 const writeFavorites = fs.createWriteStream('seedFavorites.csv');
 writeFavorites.write('id;favorite\n', 'utf8');
-const generateRandomFavoriteData = (writer, encoding, callback) => {
+const generateRandomFavoritesData = (writer, encoding, callback) => {
   let id = 0;
   let i = numOfUsers;
   function write() {
@@ -149,41 +149,48 @@ const generateRandomFavoriteData = (writer, encoding, callback) => {
   }
   write();
 };
-generateRandomFavoriteData(writeFavorites, 'utf8', (err) => {
+generateRandomFavoritesData(writeFavorites, 'utf8', (err) => {
   if (err) {
     console.log('err', err);
   } else {
-    console.log('favorite data generated');
+    console.log('favorites data generated');
     writeFavorites.end();
   }
 });
 
-// const generateRandomFavoritesData = (numOfListings, numOfUsers) => {
-//   let randomFavoritesString = '';
-//   for (let i = 1; i <= numOfUsers; i++) {
-//     let randomAmountFavorites = Math.ceil(Math.random() * 3);
-//     for (let j = 0; j < randomAmountFavorites; j++) {
-//       let randomListing = Math.ceil(Math.random() * numOfListings);
-//       (i === numOfUsers && j === randomAmountFavorites - 1) ? randomFavoritesString += `${i};${randomListing}` : randomFavoritesString += `${i};${randomListing};`;
-//     }
-//   }
-//   return randomFavoritesString;
-// };
-
-// const generateRandomSimilarsData = (numOfListings) => {
-//   let randomSimilarsString = '';
-//   for (let i = 1; i <= numOfListings; i++) {
-//     let randomAmountListings = Math.ceil(Math.random() * 5);
-//     for (let j = 0; j < randomAmountListings; j++) {
-//       let randomListing = Math.ceil(Math.random() * numOfListings);
-//       (i === numOfListings && j === randomAmountListings - 1) ? randomSimilarsString += `${i};${randomListing}` : randomSimilarsString += `${i};${randomListing};`;
-//     }
-//   }
-//   return randomSimilarsString;
-// }
-
-//   const favoritesCsvData = generateRandomFavoritesData(numOfListings, numOfUsers);
-//   const similarsCsvData = generateRandomSimilarsData(numOfListings);
-//   // createCsv('seedFavorites.csv', favoritesCsvData);
-//   // createCsv('seedSimilars.csv', similarsCsvData);
-// };
+const writeSimilars = fs.createWriteStream('seedSimilars.csv');
+writeSimilars.write('id;similar\n', 'utf8');
+const generateRandomSimilarsData = (writer, encoding, callback) => {
+  let id = 0;
+  let i = numOfListings;
+  function write() {
+    let ok = true;
+    do {
+      i--;
+      id++;
+      let data = '';
+      let randomAmountListings = Math.ceil(Math.random() * 7) + 4;
+      for (let j = 0; j < randomAmountListings; j++) {
+        let randomListing = Math.ceil(Math.random() * numOfListings);
+        data += `${id};${randomListing}\n`;
+      }
+      if (i === 0) {
+        writer.write(data, encoding, callback)
+      } else {
+        ok = writer.write(data, encoding);
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+      writer.once('drain', write);
+    }
+  }
+  write();
+};
+generateRandomSimilarsData(writeSimilars, 'utf8', (err) => {
+  if (err) {
+    console.log('err', err);
+  } else {
+    console.log('similars data generated');
+    writeSimilars.end();
+  }
+});
