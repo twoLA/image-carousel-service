c29c37c557ee68b1bd818e0d97619ef049c406b9
Linh Dang <linh.dang01@gmail.com>
2020-12-08 10:59:23 -0800

    modified cassandra schema to separate users and favorites tables

--- db/cassandra.js
+++ db/cassandra.js
@@ -4,7 +4,7 @@ const cassandra = require('cassandra-driver');
 const client = new cassandra.Client({
   contactPoints: ['localhost'],
   localDataCenter: 'datacenter1',
-  keyspace: 'carousel',
+  keyspace: 'listings',
 });
 
 // -------------------------------------------- SCHEMA ----------------------------------------------
@@ -30,6 +30,18 @@ CREATE TABLE favorites (
   user_id int,
   PRIMARY KEY (favorite_id)
 );
+CREATE TABLE users (
+  id int,
+  favorite_id int,
+  name text,
+  user_id int,
+  PRIMARY KEY (user_id)
+);
+
+// select similar_id from listings where listing_id = 100;
+// INSERT INTO listings (id, listing_id, price, bedrooms, baths, sq_footage, address, neighborhood, image, similar_id) VALUES (597, 101, 1, 2, 3, 10000, '123 main', 'sf', 'image', 50);
+// update listings set similar_id = 51 where listing_id = 101;
+// delete from listings where listing_id = 101;
 
 // ---------------------------------------- Querying Cassandra ----------------------------------------
 // const query1 = 'INSERT INTO listing (id, price, bedrooms, baths, sq_footage, neighborhood) VALUES (?, ?, ?, ?, ?, ?)';
@@ -54,7 +66,15 @@ CREATE TABLE favorites (
 //   }
 // });
 
-// select similar_id from listings where listing_id = 100;
-// INSERT INTO listings (id, listing_id, price, bedrooms, baths, sq_footage, address, neighborhood, image, similar_id) VALUES (597, 101, 1, 2, 3, 10000, '123 main', 'sf', 'image', 50);
-// update listings set similar_id = 51 where listing_id = 101;
-// delete from listings where listing_id = 101;
+// -------------------------------- Seed data into Cassandra -------------------------------------
+/* CASSANDRA DOES NOT ACCEPT COPY AS A QUERY EXECUTE THROUGH CASSANDRA DRIVER */
+// const queryListingsCopy = `COPY listings (id, listing_id, price, bedrooms, baths, sq_footage, address, neighborhood, image, similar_id) from '/Users/annlinh/Documents/HRSF131/twoLA-image-carousel-service/seedListingsCassandra.csv' with delimiter = ';' and header = true;`;
+// const queryFavoritesCopy = `COPY favorites (id, user_id, name, favorite_id) from '/Users/annlinh/Documents/HRSF131/twoLA-image-carousel-service/seedFavoritesCassandra.csv' with delimiter = ';' and header = true;`;
+
+// console.log('start cassandra database seeding', new Date());
+
+// client.execute(queryListingsCopy, [])
+//   .then(result => console.log('listings data seeded', result, new Date()))
+//   .then(client.execute(queryFavoritesCopy, []))
+//   .then(result => console.log('favorites data seeded', result, new Date()))
+//   .catch(err => console.log('error', err));
