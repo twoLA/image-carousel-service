c1523869a79cb413feb5b943a3dbb10cc919a22e
Linh Dang <linh.dang01@gmail.com>
2020-12-09 11:12:59 -0800

    added bulk load syntax for cassandra

--- db/cassandra.js
+++ db/cassandra.js
@@ -10,6 +10,19 @@ const client = new cassandra.Client({
 // -------------------------------------------- SCHEMA ----------------------------------------------
 // KEYPSPACE and CREATE TABLE must be completed in Cassandra
 // CREATE KEYSPACE listings WITH replication = { 'class': 'SimpleStrategy', 'replication_factor': 1 };
+CREATE TABLE similars (
+  id int,
+  listing_id int,
+  price int,
+  bedrooms smallint,
+  baths smallint,
+  sq_footage int,
+  address text,
+  neighborhood text,
+  image text,
+  similar_id int,
+  PRIMARY KEY ((listing_id), similar_id)
+);
 CREATE TABLE listings (
   id int,
   listing_id int,
@@ -28,14 +41,14 @@ CREATE TABLE favorites (
   favorite_id int,
   name text,
   user_id int,
-  PRIMARY KEY (favorite_id)
+  PRIMARY KEY ((favorite_id), id)
 );
 CREATE TABLE users (
   id int,
   favorite_id int,
   name text,
   user_id int,
-  PRIMARY KEY (user_id)
+  PRIMARY KEY ((user_id), id)
 );
 
 // select similar_id from listings where listing_id = 100;
@@ -55,8 +68,8 @@ CREATE TABLE users (
 //   }
 // });
 
-// const query2 = 'SELECT * FROM listing WHERE id = ?';
-// const values2 = [1];
+// const query2 = 'SELECT similar_id FROM listings WHERE listing_id = ?';
+// const values2 = [900000];
 
 // client.execute(query2, values2, { prepare: true }, (err, res) => {
 //   if (err) {
@@ -68,8 +81,16 @@ CREATE TABLE users (
 
 // -------------------------------- Seed data into Cassandra -------------------------------------
 /* CASSANDRA DOES NOT ACCEPT COPY AS A QUERY EXECUTE THROUGH CASSANDRA DRIVER */
-// const queryListingsCopy = `COPY listings (id, listing_id, price, bedrooms, baths, sq_footage, address, neighborhood, image, similar_id) from '/Users/annlinh/Documents/HRSF131/twoLA-image-carousel-service/seedListingsCassandra.csv' with delimiter = ';' and header = true;`;
-// const queryFavoritesCopy = `COPY favorites (id, user_id, name, favorite_id) from '/Users/annlinh/Documents/HRSF131/twoLA-image-carousel-service/seedFavoritesCassandra.csv' with delimiter = ';' and header = true;`;
+// const querySimilarsCopy = `COPY similars (id, listing_id, price, bedrooms, baths, sq_footage, address, neighborhood, image, similar_id) from '/Users/annlinh/Documents/HRSF131/twoLA-image-carousel-service/seedListingsCassandra.csv' with delimiter = ';' and header = true AND maxattempts=10 AND MINBATCHSIZE=1 AND MAXBATCHSIZE=8 AND PAGESIZE=10 and skipcols = 'price, bedrooms, baths, sq_footage, address, neighborhood, image';`;
+// const queryListingsCopy = `COPY listings (id, listing_id, price, bedrooms, baths, sq_footage, address, neighborhood, image, similar_id) from '/Users/annlinh/Documents/HRSF131/twoLA-image-carousel-service/seedListingsCassandra.csv' with delimiter = ';' and header = true AND maxattempts=10 AND MINBATCHSIZE=1 AND MAXBATCHSIZE=8 AND PAGESIZE=10 and skipcols = 'similar_id';`;
+// const queryFavoritesCopy = `COPY favorites (id, user_id, name, favorite_id) from '/Users/annlinh/Documents/HRSF131/twoLA-image-carousel-service/seedFavoritesCassandra.csv' with delimiter = ';' and header = true AND maxattempts=10 AND MINBATCHSIZE=1 AND MAXBATCHSIZE=8 AND PAGESIZE=10 and skipcols = 'name';`;
+// const queryUsersCopy = `COPY users (id, user_id, name, favorite_id) from '/Users/annlinh/Documents/HRSF131/twoLA-image-carousel-service/seedFavoritesCassandra.csv' with delimiter = ';' and header = true AND maxattempts=10 AND MINBATCHSIZE=1 AND MAXBATCHSIZE=8 AND PAGESIZE=10;`;
+
+// BULK LOAD USING dsbulk
+// similars - $ bin/dsbulk load -url /Users/annlinh/Documents/HRSF131/twoLA-image-carousel-service/seedListingsCassandra.csv -delim ';' -k listings -t listings -m '* = -price, -bedrooms, -baths, -sq_footage, -address, -neighborhood, -image';
+// listings - $ bin/dsbulk load -url /Users/annlinh/Documents/HRSF131/twoLA-image-carousel-service/seedListingsCassandra.csv -delim ';' -k listings -t listings -m '* = -similar_id';
+// favorites - $ bin/dsbulk load -url /Users/annlinh/Documents/HRSF131/twoLA-image-carousel-service/seedFavoritesCassandra.csv -delim ';' -k listings -t favorites -m '* = -name';
+// users - $ bin/dsbulk load -url /Users/annlinh/Documents/HRSF131/twoLA-image-carousel-service/seedFavoritesCassandra.csv -delim ';' -k listings -t users;
 
 // console.log('start cassandra database seeding', new Date());
 
