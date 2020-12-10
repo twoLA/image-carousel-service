299f1fb128d47a6314e7e32bd5654b4af5834950
Linh Dang <linh.dang01@gmail.com>
2020-12-10 12:46:50 -0800

    refactored GET and POST api for postgresql

--- server/routers/listing.js
+++ server/routers/listing.js
@@ -1,77 +1,93 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable no-console */
-const db = require('../../db/index.js');
+const db = require('../../db/postgreSQL/postgreSQL.js');
 
 // Get request for all the similar homes associated with a listing
 module.exports = {
   getOne: (req, res) => {
-    const request = req.params.id;
-    console.log('request', request);
-    db.Listing.find({ id: request }, (err, data) => {
+    const query = {
+      text: 'SELECT * FROM listings WHERE id IN (SELECT similar_id FROM similars WHERE id = $1)',
+      values: [req.params.id],
+    };
+    db.query(query, (err, data) => {
       if (err) {
-        console.log('Error getting data');
-        res.sendStatus(404);
-      } else if (data.length === 0) {
+        console.log('error for GET request', err);
         res.sendStatus(404);
       } else {
-        // console.log(data[0]._doc.similar) - gets only arr of similar listings
-        res.json(data[0]._doc.similar);
+        console.log('success for GET request');
+        res.json(data.rows);
       }
     });
   },
   postOne: (req, res) => {
-    const postId = req.params.id;
-    const postSimilars = req.body.similar;
-    console.log('id', postId, 'similar', postSimilars);
-    db.Listing.create({ id: postId, similar: postSimilars }, (err) => {
+    const param = req.body;
+    const queryInsertListing = {
+      text: 'INSERT INTO listings (price, bedrooms, baths, sq_footage, address, neighborhood, image) VALUES ($1, $2, $3, $4, $5, $6, $7)',
+      values: [param.price, param.bedrooms, param.baths,
+        param.sq_footage, param.address, param.neighborhood, param.image],
+    };
+    db.query(queryInsertListing, (err, data) => {
       if (err) {
-        console.log('Error posting data');
+        console.log('error for create listing', err);
         res.sendStatus(400);
       } else {
-        res.sendStatus(201);
-      }
-    });
-  },
-  deleteOne: (req, res) => {
-    const deleteId = req.params.id;
-    console.log('id', deleteId);
-    db.Listing.deleteOne({ id: deleteId }, (err) => {
-      if (err) {
-        console.log('Error deleting data');
-        res.sendStatus(404);
-      } else {
-        res.sendStatus(204);
-      }
-    });
-  },
-  update: async (req, res) => {
-    const updateId = req.params.id;
-    const updateSimilars = req.body.similar;
-    console.log('id', updateId, 'similar', updateSimilars);
-    await db.Listing.update({ id: updateId }, { similar: updateSimilars }, (err) => {
-      if (err) {
-        console.log(err);
-        res.sendStatus(400);
-      } else {
+        console.log('success for create listing');
+        for (let i = 0; i < 11; i++) {
+          const random = Math.floor(Math.random() * 10000000);
+          const randomInsertSimilars = {
+            text: "INSERT INTO similars (id, similar_id) VALUES ((select setval ('listings_id_seq', (select max(id) from listings))), $1)",
+            values: [random],
+          };
+          db.query(randomInsertSimilars, (error) => {
+            if (error) {
+              console.log('error for create similars');
+              res.sendStatus(400);
+            } else {
+              console.log('success for create similars');
+            }
+          });
+        }
         res.sendStatus(201);
       }
     });
   },
 };
-
-// const express = require('express');
-// const router = express.Router();
-
-// router.route('/api/listings')
-//   .get((req, res) => {
-//     db.findAll((err, data) => {
+//   postOne: (req, res) => {
+//     const postId = req.params.id;
+//     const postSimilars = req.body.similar;
+//     console.log('id', postId, 'similar', postSimilars);
+//     db.Listing.create({ id: postId, similar: postSimilars }, (err) => {
 //       if (err) {
-//         res.sendStatus(404);
+//         console.log('Error posting data');
+//         res.sendStatus(400);
 //       } else {
-//         res.json(data);
-//         res.end();
+//         res.sendStatus(201);
 //       }
 //     });
-//   });
-
-// module.exports = router;
+//   },
+//   deleteOne: (req, res) => {
+//     const deleteId = req.params.id;
+//     console.log('id', deleteId);
+//     db.Listing.deleteOne({ id: deleteId }, (err) => {
+//       if (err) {
+//         console.log('Error deleting data');
+//         res.sendStatus(404);
+//       } else {
+//         res.sendStatus(204);
+//       }
+//     });
+//   },
+//   update: async (req, res) => {
+//     const updateId = req.params.id;
+//     const updateSimilars = req.body.similar;
+//     console.log('id', updateId, 'similar', updateSimilars);
+//     await db.Listing.update({ id: updateId }, { similar: updateSimilars }, (err) => {
+//       if (err) {
+//         console.log(err);
+//         res.sendStatus(400);
+//       } else {
+//         res.sendStatus(201);
+//       }
+//     });
+//   },
+// };
--- server/server.js
+++ server/server.js
@@ -6,10 +6,10 @@ const path = require('path');
 
 const app = express();
 const port = 8030;
-const mongoose = require('mongoose');
 const listingRouter = require('./routers/listing.js');
+const db = require('../db/postgreSQL/postgreSQL.js');
 
-mongoose.connect('mongodb://localhost:/similarhomes');
+db.connect();
 
 const publicDir = path.join(__dirname, '../client/dist');
 
@@ -19,9 +19,9 @@ app.use('/carousel/listing/:id', express.static(publicDir));
 
 // get all similar listings when given a specific id
 app.get('*/:id/similars', listingRouter.getOne);
-app.post('*/:id/similars', listingRouter.postOne);
-app.delete('*/:id/similars', listingRouter.deleteOne);
-app.put('*/:id/similars', listingRouter.update);
+app.post('*/', listingRouter.postOne);
+// app.delete('*/:id/', listingRouter.deleteOne);
+// app.put('*/:id/', listingRouter.update);
 
 app.listen(8030, () => {
   console.log(`listening on http://localhost:${port}`);
